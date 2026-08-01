05d078dd37b9ed4b0fce2e9d69708a1e2f0f5fb9
sammieGHA <oo650869@gmail.com>
2026-08-01 14:25:48 -0500

    Added a 'regenerate()' method to Tiles

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import openfl.ui.Keyboard;
 import openfl.display.Sprite;
 import openfl.display.Tilemap;
 import openfl.display.Tileset;
@@ -7,33 +8,21 @@ import openfl.display.Tile;
 import openfl.Assets;
 import openfl.geom.Rectangle;
 
+import openfl.events.KeyboardEvent;
+
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
 
-		trace("Testing testing..");
+		var tiles = new Tiles(stage.stageWidth, stage.stageHeight);
+		addChild(tiles);
+		tiles.regenerate();
 
-		var bm = Assets.getBitmapData('res/tiles.png');
-		var tileset = new Tileset(bm);
-
-		for (i in 0...4) {
-			tileset.addRect(new Rectangle(i * 64, 0, 64, 64));
-		}
-
-		var tilemap = new Tilemap(stage.stageWidth, stage.stageHeight, tileset);
-		addChild(tilemap);
-
-		var cols = Math.ceil(stage.stageWidth / 64);
-		var rows = Math.ceil(stage.stageHeight / 64);
-
-		for (row in 0...rows) {
-			for (column in 0...cols) {
-				var tileID = Std.random(4);
-				var tile = new Tile(tileID, column * 64, row * 64);
-				tilemap.addTile(tile);
-			}
-		}
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, (e: KeyboardEvent) -> {
+			if (e.keyCode == Keyboard.SPACE)
+				tiles.regenerate();
+		});
 	}
 }
---
+++ src/Tiles.hx
@@ -0,0 +1,47 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.display.Tilemap;
+import openfl.display.Tileset;
+import openfl.display.Tile;
+import openfl.Assets;
+import openfl.geom.Rectangle;
+
+class Tiles extends Sprite
+{
+    var tilemap: Tilemap;
+    var cols: Int;
+    var rows: Int;
+
+	public function new(width: Int, height: Int)
+	{
+		super();
+
+		var bm = Assets.getBitmapData('res/tiles.png');
+		var tileset = new Tileset(bm);
+
+		for (i in 0...4) {
+			tileset.addRect(new Rectangle(i * 64, 0, 64, 64));
+		}
+
+		tilemap = new Tilemap(width, height, tileset);
+
+		cols = Math.ceil(width / 64);
+		rows = Math.ceil(height / 64);
+
+        addChild(tilemap);
+        regenerate();
+	}
+
+    public function regenerate() {
+        tilemap.removeTiles();
+
+		for (row in 0...rows) {
+			for (column in 0...cols) {
+				var tileID = Std.random(4);
+				var tile = new Tile(tileID, column * 64, row * 64);
+				tilemap.addTile(tile);
+			}
+		}
+    }
+}
